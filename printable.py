b52de882e0fa80ea32155788f38f91a1870cf27c
Braden Goodwin <6876787+bradentg@users.noreply.github.com>
2024-11-29 13:53:19 -0500

    Printing names

--- printable.py
+++ printable.py
@@ -2,18 +2,43 @@
 # as a physical backup
 
 import os
+import json
+import argparse
 from fpdf import FPDF
 
-def output(file_name: str):
-    path = "./output"
-    if not os.path.exists(path):
-        os.mkdir(path)
-    pdf.output(f"{path}/{file_name}")
+class PDF(FPDF):
+    def header(self):
+        self.set_font("helvetica", style="B", size=16)
+        self.cell(40, 10, "Bitwarden")
+        self.ln(20)
 
-pdf = FPDF(format="Letter")
+    def footer(self):
+        self.set_y(-15)
+        self.set_font("helvetica", style="I", size=8)
+        self.cell(0, 10, f"Page {self.page_no()}/{{nb}}", align="C")
+
+    def gen_output(self, file_name: str):
+        path = "output"
+        if not os.path.isdir(path):
+            os.mkdir(path)
+        self.output(f"{path}/{file_name}")
+
+# Get cmd args
+parser = argparse.ArgumentParser(description="Generate readable printout from Bitwarden JSON")
+parser.add_argument('filename')
+args = parser.parse_args()
+
+# Parse JSON file
+filename = args.filename
+with open(filename, 'r') as file:
+    data = json.load(file)
+
+
+# Define PDF object
+pdf = PDF()
 pdf.add_page()
-pdf.set_margins(25.4, 25.4, 25.4)
-pdf.set_font("helvetica", style="B", size=16)
-pdf.cell(40, 10, "Bitwarden")
-pdf.cell(40, 10, "next")
-output("tuto1.pdf")
+pdf.set_font("Times", size=12)
+
+for item in data["items"]:
+    pdf.cell(0, 10, item["name"], new_x="LMARGIN", new_y="NEXT")
+pdf.gen_output("test3.pdf")
